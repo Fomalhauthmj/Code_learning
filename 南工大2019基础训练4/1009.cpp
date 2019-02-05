c772caa04f11243c6efcb7d691ffcb5ac6f12fc7
Hemengjie <njtech_hemengjie@qq.com>
2019-02-05 14:54:39 +0800

    2019/2/5

--- 南工大2019基础训练4/1009.cpp
+++ 南工大2019基础训练4/1009.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include<iostream>
+using namespace std;
 void StringInGrid(int width, int height, const char* s)
 {
   int i,k;
@@ -7,35 +9,38 @@ void StringInGrid(int width, int height, const char* s)
   strcpy(buf, s);
   if(strlen(s)>width-2) buf[width-2]=0;
   
-  printf("\"+\"");
-  for(i=0;i<width-2;i++) printf(\"-\");
-  printf(\"+\\n\");
+  printf("+");
+  for(i=0;i<width-2;i++) printf("-");
+  printf("+\n");
   
   for(k=1; k<(height-1)/2;k++){
-    printf(\"|\");
-    for(i=0;i<width-2;i++) printf(\" \");
-    printf(\"|\\n\");
+    printf("|");
+    for(i=0;i<width-2;i++) printf(" ");
+    printf("|\n");
   }
   
-  printf(\"|\");
+  printf("|");
   
-  printf(\"%*s%s%*s\",_______________); //填空
-   
-  printf(\"|\\n\");
+  printf("%*s%s%*s",(width-2-strlen(buf))/2,"",buf,width-2-strlen(buf)-(width-strlen(buf)-2)/2,""); //填空
+   //空白 字符 空白
+   //(width-2-strlen(s))/2," ",s,width-2-strlen(buf)-(width-strlen(buf)-2)/2," "
+  printf("|\n");
   
   for(k=(height-1)/2+1; k<height-1; k++){
-    printf(\"|\");
-    for(i=0;i<width-2;i++) printf(\" \");
-    printf(\"|\\n\");
+    printf("|");
+    for(i=0;i<width-2;i++) printf(" ");
+    printf("|\n");
   }  
   
-  printf(\"+\");
-  for(i=0;i<width-2;i++) printf(\"-\");
-  printf(\"+\\n\");  
+  printf("+");
+  for(i=0;i<width-2;i++) printf("-");
+  printf("+\n");  
 }
 
 int main()
 {
-  StringInGrid(20,6,\"abcd1234\");
+  StringInGrid(20,6,"abcd12");
+  cout<<"(width-2-strlen(buf))/2,\"\",buf,width-2-strlen(buf)-(width-strlen(buf)-2)/2,\"\""<<endl;
+  system("pause");
   return 0;
 }
