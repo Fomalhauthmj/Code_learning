1f4502ebe8b1d0bffd45ffe92103dccb05277a92
Hemengjie <njtech_hemengjie@qq.com>
2019-02-24 13:53:56 +0800

    2019/2/24

--- codeforces/Codeforces Round #539 (Div. 2)/C.cpp
+++ codeforces/Codeforces Round #539 (Div. 2)/C.cpp
@@ -1,38 +1,26 @@
+#include <cstring>
 #include <iostream>
 using namespace std;
 #define N 300030
 long long n;
 int a[N]; //0-2^20->10^6
-int xors[N];
-void test() //TLE
+int cnt[2][1 << 21];
+void Solve()
 {
-    long long ans = 0;
-    for (int i = 1; i <= n; i++) //left
+    //两种情况 左奇 右偶  或者 左偶 右奇
+    //统计
+    long long res = 0;//结果
+    long long xors = 0;//前缀异或值
+    cnt[0][0] = 1;//初始化 对应样例是2 2 异或值为0时存在一组解 所以cnt[0][0]=1
+    //cnt[1][0]无需 因为只有2  不存在解
+    for (int i = 1; i <= n; i++)
     {
-        //You should consider only pairs where r−l+1 is even number.
-        for (int j = i + 1; j <= n; j += 2) //right
-        {
-            //int mid = (i + j - 1) / 2;
-            /*
-            al^al+1^...^amid=amid+1^amid+2^...^ar
-            a1^a2^...^amid=a1^a2^...^al-1 ^ amid+1^...^ar
-            xors[mid]=xors[l-1]^xors[r]^xors[mid]
-            */
-            //if (xors[mid] == (xors[i - 1] ^ xors[j] ^ xors[mid]))
-            if (0 == (xors[i - 1] ^ xors[j]))
-            {
-                //cout << i << " " << j << endl;
-                ans++;
-            }
-            /*
-            else if(xors[mid]==(xors[j]^xors[mid])&&i==1)//i=1
-            {
-                cout << i << " " << j << endl;
-            }
-            */
-        }
+        xors ^= a[i];//更新异或值
+        res += cnt[i%2][xors];//结果增加 之前的同奇偶同异或值计数值
+        //对应xors[i-1]==xors[j] 所以同奇偶
+        cnt[i % 2][xors]++;//更新计数值
     }
-    cout << ans << endl;
+    cout << res << endl;
 }
 int main()
 {
@@ -40,14 +28,11 @@ int main()
     cin.tie(0);
     while (cin >> n)
     {
+        memset(cnt, 0, sizeof(cnt));
         for (int i = 1; i <= n; i++)
         {
             cin >> a[i];
-            if (i == 1)
-                xors[i] = a[i];
-            else
-                xors[i] = xors[i - 1] ^ a[i];
         }
-        test();
+        Solve(); //O(n)
     }
 }
