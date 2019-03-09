057969ee649890445d836d9d0521676095c006eb
Hemengjie <njtech_hemengjie@qq.com>
2019-03-09 21:57:49 +0800

    2019/3/9

--- 蓝桥/日期问题.cpp
+++ 蓝桥/日期问题.cpp
@@ -56,6 +56,8 @@ bool judge(int year, int m, int day)
         year = 2000 + year;
     else if (year >= 60)
         year = 1900 + year;
+    if (year < 1960 || year > 2059)
+        return false;
     if (m == 2 && is_leapyear(year))
     {
         if (day >= 1 && day <= month[2] + 1)
@@ -81,10 +83,10 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0);
     //一个日期，格式是"AA/BB/CC"。  (0 <= A, B, C <= 9)
-    while (scanf("%d/%d/%d", &a[0], &a[1], &a[2]))
-    {
-        v.clear();
-        /*
+    scanf("%d/%d/%d", &a[0], &a[1], &a[2]);
+    //不要搞while循环输入！！！
+    v.clear();
+    /*
         sort(a, a + 3);
         do
         {
@@ -92,25 +94,24 @@ int main()
                 v.push_back(node(a[0], a[1], a[2]));
         } while (next_permutation(a, a + 3));
         */
-        //有采用年 / 月 / 日的，有采用月 / 日 / 年的，还有采用日 / 月 / 年
-        if (judge(a[0], a[1], a[2]))
-        {
-            v.push_back(node(a[0], a[1], a[2]));
-        }
-        if (judge(a[2], a[0], a[1]))
-        {
-            v.push_back(node(a[2], a[0], a[1]));
-        }
-        if (judge(a[2], a[1], a[0]))
-        {
-            v.push_back(node(a[2], a[1], a[0]));
-        }
-        sort(v.begin(), v.end(), cmp);
-        int cnt = unique(v.begin(), v.end()) - v.begin();
-        //输出若干个不相同的日期，每个日期一行，格式是 "yyyy-MM-dd"。多个日期按从早到晚排列。
-        for (int i = 0; i < cnt; i++)
-        {
-            printf("%04d-%02d-%02d\n", v[i].year, v[i].month, v[i].day);
-        }
+    //有采用年 / 月 / 日的，有采用月 / 日 / 年的，还有采用日 / 月 / 年
+    if (judge(a[0], a[1], a[2]))
+    {
+        v.push_back(node(a[0], a[1], a[2]));
+    }
+    if (judge(a[2], a[0], a[1]))
+    {
+        v.push_back(node(a[2], a[0], a[1]));
+    }
+    if (judge(a[2], a[1], a[0]))
+    {
+        v.push_back(node(a[2], a[1], a[0]));
+    }
+    sort(v.begin(), v.end(), cmp);
+    int cnt = unique(v.begin(), v.end()) - v.begin();
+    //输出若干个不相同的日期，每个日期一行，格式是 "yyyy-MM-dd"。多个日期按从早到晚排列。
+    for (int i = 0; i < cnt; i++)
+    {
+        printf("%04d-%02d-%02d\n", v[i].year, v[i].month, v[i].day);
     }
 }
