8dcb7c27f2745ce26f5780953bbe5382cd6602f3
Hemengjie <njtech_hemengjie@qq.com>
2019-04-26 23:13:52 +0800

    2019/4/26

--- 2050/冰水挑战.cpp
+++ 2050/冰水挑战.cpp
@@ -1,26 +1,12 @@
+#include <cstring>
 #include <iostream>
 using namespace std;
 #define N 1010
 long long n, C;
 long long a[N], b[N], c[N];
-int ans;
-int cnt;
+long long dp[N][N];
 //如何降低回溯复杂度
-void Search(long long cur_x, int pre)
-{
-    if (cur_x <= 0)
-    {
-        ans = max(ans, cnt);
-        return;
-    }
-    for (int i = pre + 1; i < n; i++)
-    {
-        cnt++;
-        Search(min(cur_x, b[i]) - a[i] + c[i], i);
-        cnt--;
-        Search(cur_x + c[i], i);
-    }
-}
+//http://acm.hdu.edu.cn/showproblem.php?pid=6495
 int main()
 {
     ios::sync_with_stdio(false);
@@ -30,15 +16,45 @@ int main()
     while (T--)
     {
         cin >> n >> C;
-        for (int i = 0; i < n; i++)
+        for (int i = 1; i <= n; i++)
         {
             cin >> a[i] >> b[i] >> c[i];
         }
         //读入数据
-        //DP? 体力X
-        cnt = 0;
-        ans = 0;
-        Search(C, -1);
+        //DP
+        memset(dp, 0, sizeof(dp));
+        dp[0][0] = C; //初始化
+        //dp[i][j]表示前i个中完成j个的最大体力
+        /*
+        受第 i 个挑战会让你丧失 ai点体力，在挑战它之前你的体力 x 会变成 min(x,bi)，
+        当你完成这个挑战的时候，你的体力会变成 x−ai，体力任何时候不允许小于等于 0，
+        无论你是否接受第 i 个挑战，在这个挑战结束以后你的体力都会增加 ci。
+        */
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                if (dp[i][j] > 0)
+                {
+                    //接受第i+1个
+                    if (min(dp[i][j], b[i + 1]) > a[i + 1])
+                    {
+                        dp[i + 1][j + 1] = max(dp[i + 1][j + 1], min(dp[i][j], b[i + 1]) - a[i + 1] + c[i + 1]);
+                    }
+                    //不接受
+                    dp[i + 1][j] = max(dp[i + 1][j], dp[i][j] + c[i + 1]);
+                }
+            }
+        }
+        int ans=0;
+        for (int j = n; j >= 0; j--)
+        {
+            if (dp[n][j] > 0)
+            {
+                ans = j;
+                break;
+            }
+        }
         cout << ans << endl;
     }
     //system("pause");
