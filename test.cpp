3822eb954856d9a6b978b998417664109ed14927
Hemengjie <njtech_hemengjie@qq.com>
2019-04-30 12:35:17 +0800

    2019/4/30

--- test.cpp
+++ test.cpp
@@ -1,35 +1,11 @@
-#include <algorithm>
 #include <iostream>
-#include <map>
-#include <string>
+#include <queue>
 #include <vector>
 using namespace std;
-map<string, int> m;
 int main()
 {
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    string line;
-    while (1)
-    {
-        getline(cin, line);
-        if (line == "break")
-            break;
-        m[line]++;
-    }
-    int sum = 0;
-    vector<pair<int, string>> v;
-    for (auto ele : m)
-    {
-        cout << ele.first << " " << ele.second << endl;
-        sum += ele.second;
-        v.push_back(make_pair(ele.second, ele.first));
-    }
-    cout << "SUM:" << sum << endl;
-    sort(v.begin(), v.end());
-    for (auto ele : v)
-    {
-        cout << ele.first << " " << ele.second << endl;
-    }
-    return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout << "我??！" << endl;
+    system("pause");
 }
