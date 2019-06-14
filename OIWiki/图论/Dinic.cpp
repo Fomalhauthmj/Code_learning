f4600bfdcc5a2f7ac51ac5a5499456057cff2782
Hemengjie <njtech_hemengjie@qq.com>
2019-06-14 22:44:08 +0800

    2019/6/14

--- OIWiki/图论/Dinic.cpp
+++ OIWiki/图论/Dinic.cpp
@@ -2,50 +2,51 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-#define inf 0x3f3f3f3f
-#define N 10100
-#define M 201000
+#define inf 0x3f3f3f3f3f
+#define N 1200
+#define M 245000
+#define ll long long
 //反向边的存在 实际边数的两倍
 struct Edge
 {
-    int to;
-    int w;
-    int next;
+    ll to;
+    ll w;
+    ll next;
 };
 Edge edge[M];
-int dep[N];
-int head[N];
-int cur[N]; //当前弧优化
-int cnt = 0;
-int n, m;
+ll dep[N];
+ll head[N];
+ll cur[N]; //当前弧优化
+ll cnt = 0;
+ll n, m;
 void Init()
 {
     memset(head, -1, sizeof(head));
 }
-void add(int u, int v, int w)
+void add(ll u, ll v, ll w)
 {
     edge[cnt].to = v;
     edge[cnt].w = w;
     edge[cnt].next = head[u];
     head[u] = cnt++;
 }
-void Add(int u, int v, int w)
+void Add(ll u, ll v, ll w)
 {
     add(u, v, w);
     add(v, u, 0); //反向边
 }
-int s, t;
+ll s, t;
 bool BFS()
 {
-    queue<int> q;
+    queue<ll> q;
     memset(dep, -1, sizeof(dep));
     dep[s] = 0;
     q.push(s);
     while (!q.empty())
     {
-        int now = q.front();
+        ll now = q.front();
         q.pop();
-        for (int i = head[now]; i != -1; i = edge[i].next)
+        for (ll i = head[now]; i != -1; i = edge[i].next)
         {
             if (edge[i].w > 0 && dep[edge[i].to] == -1)
             {
@@ -58,17 +59,17 @@ bool BFS()
         return false; //不存在分层图
     return true;
 }
-int DFS(int now, int flow)
+ll DFS(ll now, ll flow)
 {
     if (now == t)
         return flow;
-    int used = 0;
-    for (int &i = cur[now]; i != -1; i = edge[i].next)
+    ll used = 0;
+    for (ll &i = cur[now]; i != -1; i = edge[i].next)
     {
         //&：当前弧优化
         if (dep[edge[i].to] == dep[now] + 1 && edge[i].w)
         {
-            int inc = DFS(edge[i].to, min(flow - used, edge[i].w));
+            ll inc = DFS(edge[i].to, min(flow - used, edge[i].w));
             if (inc > 0)
             {
                 edge[i].w -= inc;
@@ -79,21 +80,20 @@ int DFS(int now, int flow)
             }
         }
     }
+    if (!used)
+        dep[now] = -1;
     return used;
 }
-int Dinic()
+ll Dinic()
 {
-    int ans = 0;
+    ll ans = 0;
     while (BFS())
     {
-        for (int i = 1; i <= n; i++)
+        for (ll i = 1; i <= n; i++)
         {
             cur[i] = head[i];
         }
-        while (int inc = DFS(s, inf))
-        {
-            ans += inc;
-        }
+        ans += DFS(s, inf);
     }
     return ans;
 }
@@ -102,9 +102,9 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0);
     cin >> n >> m >> s >> t;
-    int u, v, w;
+    ll u, v, w;
     Init();
-    for (int i = 0; i < m; i++)
+    for (ll i = 0; i < m; i++)
     {
         cin >> u >> v >> w;
         Add(u, v, w);
