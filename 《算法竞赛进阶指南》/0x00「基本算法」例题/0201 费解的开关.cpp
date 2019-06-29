e4212ea08c447da840e1d0643b0b6b4e3f9ef368
Hemengjie <njtech_hemengjie@qq.com>
2019-06-29 18:20:55 +0800

    2019/6/29

--- 《算法竞赛进阶指南》/0x00「基本算法」例题/0201 费解的开关.cpp
+++ 《算法竞赛进阶指南》/0x00「基本算法」例题/0201 费解的开关.cpp
@@ -1,24 +1,82 @@
+#include <cstring>
 #include <iostream>
+#include <string>
 using namespace std;
+int t_mat[5][5];
 int mat[5][5];
 int n;
-int Solve()
+void Change(int i, int j)
 {
+    if (i < 0 || i >= 5)
+        return;
+    if (j < 0 || j >= 5)
+        return;
+    t_mat[i][j] = t_mat[i][j] ^ 1;
+}
+int Solve(int s)
+{
+    int temp = 0;
+    memcpy(t_mat, mat, sizeof(mat));
+    for (int i = 0; i < 5; i++)
+    {
+        if (((s >> i) & 1))
+        {
+            Change(0, i);
+            Change(1, i);
+            Change(0, i + 1);
+            Change(0, i - 1);
+            temp++;
+        }
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (!t_mat[i][j])
+            {
+                temp++;
+                Change(i, j);
+                Change(i + 1, j);
+                Change(i + 1, j - 1);
+                Change(i + 1, j + 1);
+                Change(i + 2, j);
+            }
+        }
+    }
+    for (int j = 0; j < 5; j++)
+    {
+        if (!t_mat[4][j])
+            return -1;
+    }
+    return temp;
 }
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cin >> n;
+    string str;
     while (n--)
     {
         for (int i = 0; i < 5; i++)
         {
+            cin >> str;
             for (int j = 0; j < 5; j++)
-                cin >> mat[i][j];
+                mat[i][j] = str[j] - '0';
         }
-        cout << Solve() << endl;
+        int ans = 0x3f3f3f3f;
+        for (int i = 0; i < 32; i++)
+        {
+            int temp = Solve(i);
+            //cout << i << " " << temp << endl;
+            if (temp != -1)
+                ans = min(ans, temp);
+        }
+        if (ans > 6)
+            cout << -1 << endl;
+        else
+            cout << ans << endl;
     }
-    system("pause");
+    //system("pause");
     return 0;
 }
