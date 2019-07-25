8bfa32dcfbd9685da12211e58350c89c0b0bf37a
Hemengjie <njtech_hemengjie@qq.com>
2019-07-25 15:37:49 +0800

    2019/7/25

--- 南工大一九年夏/栈与队列/280B Maximum Xor Secondary.cpp
+++ 南工大一九年夏/栈与队列/280B Maximum Xor Secondary.cpp
@@ -1,20 +1,41 @@
 //http://codeforces.com/contest/280/problem/B
-//TODO 2000
-#include<iostream>
+#include <iostream>
+#include<stack>
 using namespace std;
-const int N=1E5+20;
-int s[N];
+const int N = 1E5 + 20;
+int a[N];
 int n;
+stack<int> s;
 /*
-区间最大次大 
+维护区间最大次大
+单调递减栈 存放次大数 出栈时比较  从左到右 从右到左各一遍
  */
-int main()
+int main() 
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    cin>>n;
+    cin >> n;
+    for(int i=1;i<=n;i++) cin>>a[i];
+    int ans=0;
     for(int i=1;i<=n;i++)
-        cin>>s[i];
-    system("pause");
+    {
+        while(s.size()&&s.top()<=a[i])
+        {
+            ans=max(ans,a[i]^s.top());
+            s.pop();
+        }
+        s.push(a[i]);
+    }
+    for(int i=n;i>=1;i--)
+    {
+        while(s.size()&&s.top()<=a[i])
+        {
+            ans=max(ans,a[i]^s.top());
+            s.pop();
+        }
+        s.push(a[i]);
+    }
+    cout<<ans<<endl;
+    //system("pause");
     return 0;
 }
