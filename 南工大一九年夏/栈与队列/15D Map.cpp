cce874eef562855038c13c1ad2dd7ace9623dc9f
Hemengjie <njtech_hemengjie@qq.com>
2019-07-26 23:47:22 +0800

    2019/7/26

--- 南工大一九年夏/栈与队列/15D Map.cpp
+++ 南工大一九年夏/栈与队列/15D Map.cpp
@@ -1,8 +1,8 @@
 //http://codeforces.com/contest/15/problem/D
-//TODO 2500
-#include <cstring>
+#include <algorithm>
+#include <deque>
 #include <iostream>
-#include <queue>
+#include <stdio.h>
 #include <vector>
 using namespace std;
 #define ll long long
@@ -11,91 +11,88 @@ const int N = 1010;
 int n, m, a, b;
 int map[N][N];
 bool vis[N][N];
-ll sum[N][N];
-ll ret[N * N][3];
-int tot = 0;
-ll Count(int x, int y, int min_x, int min_y)
+ll sum[N][N], min_ele[N][N], right_down[N][N];
+inline bool Judge(int x, int y)
 {
-    return sum[x + a - 1][y + b - 1] - sum[x + a - 1][y - 1] - sum[x - 1][y + b - 1] + sum[x - 1][y - 1] - a * b * map[min_x][min_y];
+    if (vis[x][y] || vis[x + a - 1][y] || vis[x + a - 1][y + b - 1] || vis[x][y + b - 1])
+        return false;
+    return true;
 }
 struct node
 {
     int x, y;
-    int min_x, min_y;
     ll count;
-    node(int i1, int i2, int i3, int i4) : x(i1), y(i2), min_x(i3), min_y(i4)
-    {
-        count = Count(x, y, min_x, min_y);
-    }
+    node(int i1, int i2, ll l1) : x(i1), y(i2), count(l1) {}
     bool operator<(const node &n) const
     {
-        if (count == n.count)
-            if (x == n.x)
-                return y > n.y;
-            else
-                return x > n.x;
-        return count > n.count;
+        if (count != n.count)
+            return count < n.count;
+        if (x != n.x)
+            return x < n.x;
+        return y < n.y;
     }
 };
-pair<int, int> Find(int x, int y)
+vector<node> task, ret;
+void Work()
 {
-    int temp = inf;
-    pair<int, int> ret;
-    for (int i = x; i < x + a; i++)
-        for (int j = y; j < y + b; j++)
-            if (map[i][j] < temp)
-            {
-                ret.first = i;
-                ret.second = j;
-                temp = map[i][j];
-            }
-    return ret;
+    //对每行进行单调递增队列 确定范围内最小
+    for (int i = 1; i <= n; i++)
+    {
+        deque<int> q; //存放列号
+        for (int j = 1; j <= m; j++)
+        {
+            while (!q.empty() && j - q.front() >= b)
+                q.pop_front();
+            while (!q.empty() && map[i][j] < map[i][q.back()])
+                q.pop_back();
+            q.push_back(j);
+            min_ele[i][j] = map[i][q.front()];
+        }
+    }
+    //对每列进行单调队列 确定范围内最小
+    for (int j = b; j <= m; j++)
+    {
+        deque<int> q;
+        for (int i = 1; i <= n; i++)
+        {
+            while (!q.empty() && i - q.front() >= a)
+                q.pop_front();
+            while (!q.empty() && min_ele[i][j] < min_ele[q.back()][j])
+                q.pop_back();
+            q.push_back(i);
+            right_down[i][j] = min_ele[q.front()][j];
+        }
+    }
 }
-bool Judge(int x, int y)
-{
-    for (int i = x; i < x + a; i++)
-        for (int j = y; j < y + b; j++)
-            if (vis[i][j])
-                return false;
-    return true;
-}
-priority_queue<node> pq;
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cin >> n >> m >> a >> b;
+    scanf("%d%d%d%d", &n, &m, &a, &b);
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= m; j++)
-            cin >> map[i][j];
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= m; j++)
-            sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + map[i][j];
+            scanf("%d", &map[i][j]), sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + map[i][j];
+    Work();
     for (int i = 1; i + a - 1 <= n; i++)
     {
         for (int j = 1; j + b - 1 <= m; j++)
         {
-            pair<int, int> t = Find(i, j);
-            pq.push(node(i, j, t.first, t.second));
+            ll temp = sum[i + a - 1][j + b - 1] - sum[i + a - 1][j - 1] - sum[i - 1][j + b - 1] + sum[i - 1][j - 1] - a * b * right_down[i + a - 1][j + b - 1];
+            task.push_back(node(i, j, temp));
         }
     }
-    while (!pq.empty())
+    sort(task.begin(), task.end());
+    for (auto it:task)
     {
-        node now = pq.top();
-        pq.pop();
-        if (Judge(now.x, now.y))
+        if (Judge(it.x, it.y))
         {
-            ret[tot][0] = now.x;
-            ret[tot][1] = now.y;
-            ret[tot++][2] = now.count;
-            for (int i = now.x; i < now.x + a; i++)
-                for (int j = now.y; j < now.y + b; j++)
+            ret.push_back(it);
+            for (int i = it.x; i < it.x + a; i++)
+                for (int j = it.y; j < it.y + b; j++)
                     vis[i][j] = 1;
         }
     }
-    cout << tot << endl;
-    for (int i = 0; i < tot; i++)
-        cout << ret[i][0] << ' ' << ret[i][1] << ' ' << ret[i][2] << endl;
+    printf("%d\n",ret.size());
+    for (auto it : ret)
+        printf("%d %d %I64d\n", it.x, it.y, it.count);
     //system("pause");
     return 0;
 }
--- 洛谷/P2216 [HAOI2007]理想的正方形.cpp
+++ 洛谷/P2216 [HAOI2007]理想的正方形.cpp
@@ -1,1 +1,11 @@
 // TODO 单调队列  维护矩阵最大值最小值
+#include<iostream>
+using namespace std;
+#define ll long long
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    system("pause");
+    return 0;
+}
