eea9a3ae6ce6fbd32561e577c7930e900075afd6
Hemengjie <njtech_hemengjie@qq.com>
2019-08-19 16:39:02 +0800

    2019/8/19

--- 洛谷/SP1811 LCS - Longest Common Substring.cpp
+++ 洛谷/SP1811 LCS - Longest Common Substring.cpp
@@ -5,20 +5,11 @@
 using namespace std;
 #define ll long long
 const int MAXLEN = 1e6 + 50;
-ll sze[MAXLEN * 2];
-ll ans;
-namespace SAM
-{
 struct state
 {
     int len, link, next[26];
 } st[MAXLEN * 2];
 int sz, last;
-int head[MAXLEN * 2], ver[MAXLEN * 2], nxt[MAXLEN * 2], tot;
-void add(int x, int y)
-{
-    ver[++tot] = y, nxt[tot] = head[x], head[x] = tot;
-}
 void SAM_Init()
 {
     st[0].len = 0, st[0].link = -1;
@@ -27,7 +18,6 @@ void SAM_Init()
 void SAM_Extend(int c)
 {
     int cur = sz++;
-    sze[cur] = 1;
     st[cur].len = st[last].len + 1;
     int p = last;
     while (p != -1 && !st[p].next[c])
@@ -58,55 +48,28 @@ void SAM_Extend(int c)
     }
     last = cur;
 }
-void BuildLinkTree()
-{
-    for (int i = 1; i < sz; i++)
-        add(st[i].link, i);
-}
-void DFS(int x)
-{
-    for (int i = head[x]; i; i = nxt[i])
-    {
-        int y = ver[i];
-        DFS(y);
-        sze[x] += sze[y];
-    }
-    if (sze[x] != 1)
-        ans = max(ans, sze[x] * st[x].len);
-}
-int id[MAXLEN * 2], c[MAXLEN * 2];
-void Topo()
-{
-    //计数排序
-    for (int i = 1; i < sz; i++)
-        c[st[i].len]++;
-    for (int i = 1; i < MAXLEN; i++)
-        c[i] += c[i - 1];
-    for (int i = 1; i < sz; i++)
-        id[c[st[i].len]--] = i;
-    for (int i = sz - 1; i >= 1; i--)
-    {
-        sze[st[id[i]].link] += sze[id[i]];
-        if (sze[id[i]] > 1)
-            ans = max(ans, sze[id[i]] * st[id[i]].len);
-    }
-}
-} // namespace SAM
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    string str;
-    cin >> str;
-    SAM::SAM_Init();
-    for (int i = 0; i < str.length(); i++)
-        SAM::SAM_Extend(str[i] - 'a');
-    ans = 0;
-    /*
-    SAM::BuildLinkTree();
-    SAM::DFS(0);
-    */
-    SAM::Topo();
+    string a, b;
+    cin >> a >> b;
+    SAM_Init();
+    for (int i = 0; i < a.length(); i++)
+        SAM_Extend(a[i] - 'a');
+    int v = 0, l = 0, ans = 0;
+    for (int i = 0; i < b.length(); i++)
+    {
+        int c = b[i] - 'a';
+        while (v && !st[v].next[c])
+        {
+            v = st[v].link;
+            l = st[v].len;
+        }
+        if (st[v].next[c])
+            v = st[v].next[c], l++;
+        ans = max(ans, l);
+    }
     cout << ans << endl;
     //system("pause");
     return 0;
--- 洛谷/SP1812 LCS2 - Longest Common Substring II.cpp
+++ 洛谷/SP1812 LCS2 - Longest Common Substring II.cpp
@@ -4,21 +4,13 @@
 #include <string>
 using namespace std;
 #define ll long long
+#define inf 0x3f3f3f3f
 const int MAXLEN = 1e6 + 50;
-ll sze[MAXLEN * 2];
-ll ans;
-namespace SAM
-{
 struct state
 {
     int len, link, next[26];
 } st[MAXLEN * 2];
 int sz, last;
-int head[MAXLEN * 2], ver[MAXLEN * 2], nxt[MAXLEN * 2], tot;
-void add(int x, int y)
-{
-    ver[++tot] = y, nxt[tot] = head[x], head[x] = tot;
-}
 void SAM_Init()
 {
     st[0].len = 0, st[0].link = -1;
@@ -27,7 +19,6 @@ void SAM_Init()
 void SAM_Extend(int c)
 {
     int cur = sz++;
-    sze[cur] = 1;
     st[cur].len = st[last].len + 1;
     int p = last;
     while (p != -1 && !st[p].next[c])
@@ -58,55 +49,56 @@ void SAM_Extend(int c)
     }
     last = cur;
 }
-void BuildLinkTree()
-{
-    for (int i = 1; i < sz; i++)
-        add(st[i].link, i);
-}
-void DFS(int x)
-{
-    for (int i = head[x]; i; i = nxt[i])
-    {
-        int y = ver[i];
-        DFS(y);
-        sze[x] += sze[y];
-    }
-    if (sze[x] != 1)
-        ans = max(ans, sze[x] * st[x].len);
-}
-int id[MAXLEN * 2], c[MAXLEN * 2];
+int c[MAXLEN * 2], id[MAXLEN * 2];
 void Topo()
 {
-    //计数排序
     for (int i = 1; i < sz; i++)
         c[st[i].len]++;
     for (int i = 1; i < MAXLEN; i++)
         c[i] += c[i - 1];
     for (int i = 1; i < sz; i++)
         id[c[st[i].len]--] = i;
-    for (int i = sz - 1; i >= 1; i--)
-    {
-        sze[st[id[i]].link] += sze[id[i]];
-        if (sze[id[i]] > 1)
-            ans = max(ans, sze[id[i]] * st[id[i]].len);
-    }
 }
-} // namespace SAM
+int max_match_len[MAXLEN * 2]; //处理单字符串时每个状态最大匹配长度
+int min_match_len[MAXLEN * 2]; //对于所有字符串每个状态的最大匹配长度
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    string str;
-    cin >> str;
-    SAM::SAM_Init();
-    for (int i = 0; i < str.length(); i++)
-        SAM::SAM_Extend(str[i] - 'a');
-    ans = 0;
-    /*
-    SAM::BuildLinkTree();
-    SAM::DFS(0);
-    */
-    SAM::Topo();
+    string a, b;
+    cin >> a;
+    SAM_Init();
+    for (int i = 0; i < a.length(); i++)
+        SAM_Extend(a[i] - 'a');
+    Topo();
+    memset(min_match_len, inf, sizeof(min_match_len));
+    while (cin >> b)
+    {
+        int v = 0, l = 0;
+        for (int i = 0; i < b.length(); i++)
+        {
+            int c = b[i] - 'a';
+            while (v && !st[v].next[c])
+            {
+                v = st[v].link;
+                l = st[v].len;
+            }
+            if (st[v].next[c])
+                v = st[v].next[c], l++;
+            max_match_len[v] = max(max_match_len[v], l);
+        }
+        for (int i = sz - 1; i >= 1; i--)
+        {
+            int s = id[i], p = st[s].link;
+            max_match_len[p] = max(max_match_len[p], min(max_match_len[s], st[p].len));
+            //这里父节点取子节点的最大匹配长度取与自身最大串长度的较小值 避免溢出
+            min_match_len[s] = min(min_match_len[s], max_match_len[s]);
+            max_match_len[s] = 0;
+        }
+    }
+    int ans = 0;
+    for (int i = 1; i < sz; i++)
+        ans = max(ans, min_match_len[i]);
     cout << ans << endl;
     //system("pause");
     return 0;
