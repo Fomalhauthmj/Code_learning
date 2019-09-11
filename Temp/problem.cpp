11d65b7bc178b3a33b774c38a67bc0a48e0d6391
Hemengjie <njtech_hemengjie@qq.com>
2019-09-11 22:38:56 +0800

    2019/9/11

--- Temp/problem.cpp
+++ Temp/problem.cpp
@@ -1,87 +1,46 @@
 #include <cstring>
 #include <iostream>
-#include <queue>
 #include <stdio.h>
 using namespace std;
-const int MAXLEN = 1e6 + 50;
-struct Trie
+const int MAXLEN = 5e5 + 50;
+char ori[MAXLEN], str[MAXLEN * 2], legal[MAXLEN * 2];
+int d1[MAXLEN * 2], n, m;
+long long ans = 0;
+void Manacher()
 {
-    int tr[MAXLEN][26], fail[MAXLEN], End[MAXLEN];
-    int sz, rt;
-    int NewNode()
+    for (int i = 0, l = 0, r = -1; i < m; i+=2)
     {
-        memset(tr[sz], 0, sizeof(tr[sz]));
-        End[sz] = 0, fail[sz] = 0;
-        return sz++;
+        int k = (i > r) ? 1 : min(d1[l + r - i], r - i);
+        while (i - k >= 0 && i + k < m && str[i - k] == legal[i + k])
+            k++;
+        d1[i] = k--;
+        if ((d1[i] - 1) % 2 == 0)
+            ans += (d1[i] - 1) / 2;
+        //cout << i << " " << (d1[i] - 1) / 2 << endl;
+        if (i + k > r)
+            l = i - k, r = i + k;
     }
-    void Init()
-    {
-        sz = 0, rt = NewNode();
-    }
-    void Insert(const char *s)
-    {
-        int p = rt;
-        for (int i = 0; s[i]; i++)
-        {
-            int c = s[i] - 'a';
-            if (!tr[p][c])
-                tr[p][c] = NewNode();
-            p = tr[p][c];
-        }
-        End[p]++;
-    }
-    void Build()
-    {
-        queue<int> q;
-        for (int c = 0; c < 26; c++)
-            if (tr[rt][c])
-                q.push(tr[rt][c]);
-        while (!q.empty())
-        {
-            int u = q.front();
-            q.pop();
-            for (int c = 0; c < 26; c++)
-            {
-                if (tr[u][c])
-                    fail[tr[u][c]] = tr[fail[u]][c], q.push(tr[u][c]);
-                else
-                    tr[u][c] = tr[fail[u]][c];
-            }
-        }
-    }
-    int Query(const char *s)
-    {
-        int u = rt, res = 0;
-        for (int i = 0; s[i]; i++)
-        {
-            u = tr[u][s[i] - 'a']; // 转移
-            for (int j = u; j && End[j] != -1; j = fail[j])
-            {
-                res += End[j], End[j] = -1;
-            }
-        }
-        return res;
-    }
-} ac;
-char str[MAXLEN];
+}
 int main()
 {
-    int T;
-    scanf("%d", &T);
-    while (T--)
+    scanf("%d", &n);
+    scanf("%s", ori);
+    str[0] = '#';
+    for (int i = 0; i < n; i++)
     {
-        int n;
-        scanf("%d", &n);
-        ac.Init();
-        for (int i = 0; i < n; i++)
-        {
-            scanf("%s", str);
-            ac.Insert(str);
-        }
-        ac.Build();
-        scanf("%s", str);
-        printf("%d\n", ac.Query(str));
+        str[(i + 1) * 2] = '#';
+        str[(i + 1) * 2 - 1] = ori[i];
     }
+    m = n * 2 + 1;
+    for (int i = 0; i < m; i++)
+        if (str[i] == '0')
+            legal[i] = '1';
+        else if (str[i] == '1')
+            legal[i] = '0';
+        else
+            legal[i] = '#';
+    Manacher();
+    printf("%lld\n", ans);
     //system("pause");
     return 0;
 }
