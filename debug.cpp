8e8133f56dc0911c015ecafec6ba4477e006ae7b
Hemengjie <njtech_hemengjie@qq.com>
2019-09-15 19:57:24 +0800

    2019/9/15

--- debug.cpp
+++ debug.cpp
@@ -1,89 +1,16 @@
-#include <cstring>
-#include <iostream>
-#include <stdio.h>
-using namespace std;
-const int N = 1e5 + 50;
-const int M = 4e5 + 50;
-#define pii pair<int, int>
-int n, m, k;
-int sze[N], fa[N];
-bool monster[N], vis[N];
-int Find(int x)
+void gauss() 
 {
-    if (fa[x] == x)
-        return x;
-    return fa[x] = Find(fa[x]);
-}
-pii road[M];
-int head[N], ver[M], nxt[M], tot, deg[N];
-void add(int x, int y)
-{
-    ver[++tot] = y, nxt[tot] = head[x], head[x] = tot;
-}
-void Init()
-{
-    tot = 0;
-    memset(deg, 0, sizeof(deg));
-    memset(head, 0, sizeof(head));
-    memset(monster, 0, sizeof(monster));
-    memset(vis, 0, sizeof(vis));
-    for (int i = 1; i <= n; i++)
-        fa[i] = i, sze[i] = 1;
-}
-
-int main()
-{
-    int T;
-    scanf("%d", &T);
-    while (T--)
+    int now = 0, to;
+    int t;
+    for (int i = 0; i < 16; i++, now++)
     {
-        scanf("%d%d%d", &n, &m, &k);
-        Init();
-        for (int i = 0; i < m; i++)
-            scanf("%d%d", &road[i].first, &road[i].second);
-        for (int i = 0, pos; i < k; i++)
-            scanf("%d", &pos), monster[pos] = true, sze[pos] = 0;
-        for (int i = 0; i < m; i++)
+        t = a[now][i];
+        for (int j = 0; j < 16 + 1; j++) a[now][j] /= t;
+        for (int j = 0; j < n; j++) 
+        if (j != now) 
         {
-            int fx = Find(road[i].first);
-            int fy = Find(road[i].second);
-            if (fx != fy && !monster[fx] && !monster[fy])
-            {
-                fa[fx] = fy;
-                sze[fy] += sze[fx];
-            }
-            else if (monster[fx] || monster[fy])
-            {
-                if (monster[fx])
-                    deg[fx]++;
-                if (monster[fy])
-                    deg[fy]++;
-                add(fx, fy);
-                add(fy, fx);
-            }
+            t = a[j][i];
+            for (int k = 0; k < 16 + 1; k++) a[j][k] -= t * a[now][k];
         }
-        int start = Find(1);
-        double ans = sze[start];
-        sze[start] = 0;
-        double temp = 0;
-        for (int i = head[start]; i; i = nxt[i])
-        {
-            int y = ver[i];
-            if (!vis[y])
-            {
-                vis[y] = 1;
-                int sum = 0;
-                for (int j = head[y]; j; j = nxt[j])
-                {
-                    int z = ver[j];
-                    sum += sze[Find(z)];
-                }
-                if (sum)
-                    temp = max(temp, sum * 1.0 / deg[y]);
-            }
-        }
-        printf("%.8lf\n", ans + temp);
     }
-    //system("pause");
-    return 0;
 }
--- 一些比赛/The Preliminary Contest for ICPC Asia Shanghai 2019/H. Luhhy's Matrix.cpp
+++ 一些比赛/The Preliminary Contest for ICPC Asia Shanghai 2019/H. Luhhy's Matrix.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 #define ui unsigned int
 ui seed, lastans;
+ui a[16][16 << 1], rev[16][16];
 ui A[16][16], R[16][16];
 ui base1[16], base2[16];
 ui ret[16][16];
@@ -13,7 +14,47 @@ struct node
     ui matrix[16][16];
 };
 deque<node> q;
-deque<node> qc;
+inline ui qpow(ui x, ui k)
+{
+    ui ans = 1;
+    while (k)
+    {
+        if (k & 1)
+            ans = ans * x % 2;
+        x = x * x % 2;
+        k >>= 1;
+    }
+    return ans % 2;
+}
+inline void Gauss_j()
+{
+    for (int i = 0, r; i < 16; ++i)
+    {
+        r = i;
+        for (int j = i + 1; j < 16; ++j)
+            if (a[j][i] > a[r][i])
+                r = j;
+        if (r != i)
+            swap(a[i], a[r]);
+        int kk = qpow(a[i][i], 2 - 2); //求逆元
+        for (int k = 0; k < 16; ++k)
+        {
+            if (k == i)
+                continue;
+            int p = a[k][i] * kk % 2;
+            for (int j = i; j < (16 << 1); ++j)
+                a[k][j] = ((a[k][j] - p * a[i][j]) % 2 + 2) % 2;
+        }
+        for (int j = 0; j < (16 << 1); ++j)
+            a[i][j] = (a[i][j] * kk % 2);
+        //更新当前行 如果放在最后要再求一次逆元,不如直接放在这里
+    }
+    for (int i = 0; i < 16; ++i)
+    {
+        for (int j = 16; j < (16 << 1); ++j)
+            rev[i][j - 16] = a[i][j];
+    }
+}
 void Init()
 {
     base1[0] = base2[0] = 1;
@@ -59,7 +100,7 @@ ui Ret()
 int main()
 {
     freopen("input", "r", stdin);
-    freopen("output2", "w", stdout);
+    freopen("output1", "w", stdout);
     int T;
     scanf("%d", &T);
     Init();
@@ -67,7 +108,6 @@ int main()
     {
         while (q.size())
             q.pop_front();
-        memset(ret, 0, sizeof(ret));
         int n;
         scanf("%d", &n);
         ui t;
@@ -79,41 +119,39 @@ int main()
             {
                 Build();
                 node add;
-                if (!empty_flag)
+                if (q.empty())
                     memcpy(ret, A, sizeof(ret));
                 else
                 {
                     Mul(A, ret);
                     memcpy(ret, R, sizeof(ret));
                 }
-                empty_flag++;
                 memcpy(add.matrix, A, sizeof(add.matrix));
                 q.push_back(add);
             }
             else
             {
-                if (empty_flag)
+                if (q.size())
                 {
-                    empty_flag--;
+                    memset(a, 0, sizeof(a));
+                    for (int i = 0; i < 16; ++i)
+                        for (int j = 0; j < 16; ++j)
+                            a[i][j] = q.front().matrix[i][j], a[i][i + 16] = 1;
+                    Gauss_j();
                     q.pop_front();
-                    if (empty_flag)
+                    if (q.size() > 1)
                     {
-                        qc = q;
-                        ui T[16][16];
-                        memcpy(T, qc.back().matrix, sizeof(T));
-                        qc.pop_back();
-                        while (qc.size())
-                        {
-                            Mul(T, qc.back().matrix);
-                            qc.pop_back();
-                            memcpy(T, R, sizeof(T));
-                        }
-                        memcpy(ret, T, sizeof(ret));
+                        Mul(ret, rev);
+                        memcpy(ret, R, sizeof(ret));
+                    }
+                    else if (q.size() == 1)
+                    {
+                        memcpy(ret, q.back().matrix, sizeof(ret));
                     }
                 }
             }
             ui ans = 0;
-            if (empty_flag)
+            if (q.size())
                 ans = Ret();
             lastans = ans;
             cout << ans << endl;
