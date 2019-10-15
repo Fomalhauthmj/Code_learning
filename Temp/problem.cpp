6eae72c2b3c1d60b5fc84ec323b14ca72ce5924c
Hemengjie <njtech_hemengjie@qq.com>
2019-10-15 20:34:38 +0800

    2019/10/15

--- Temp/problem.cpp
+++ Temp/problem.cpp
@@ -1,177 +1,35 @@
-#include <cstring>
-#include <iostream>
-#include <stdio.h>
+#include<iostream>
 using namespace std;
-const int N = 1e5 + 50;
-const int M = 2e5 + 50;
+const int N=2e5+50;
 #define ll long long
-int temp[N], weight[N];
-int head[N], ver[M], nxt[M], tot;
-int fa[N], dep[N], sze[N], son[N], top[N], dfn[N], rnk[N], dfn_tot;
-struct SegmentTree
-{
-    int l, r;
-    int lc, rc, cnt;
-    int edit;
-#define ls(x) (x << 1)
-#define rs(x) (x << 1 | 1)
-#define l(x) tree[x].l
-#define r(x) tree[x].r
-#define cnt(x) tree[x].cnt
-#define lc(x) tree[x].lc
-#define rc(x) tree[x].rc
-#define edit(x) tree[x].edit
-} tree[N << 2];
-//FIXME 
-void PushUp(int rt)
-{
-    cnt(rt) = (rc(ls(rt)) == lc(rs(rt))) ? cnt(ls(rt)) + cnt(rs(rt)) - 1 : cnt(ls(rt)) + cnt(rs(rt));
-    cout << tree[rt].l << ":" << tree[rt].r << " cnt ->" << cnt(rt) << endl;
-    lc(rt) = lc(ls(rt)), rc(rt) = rc(rs(rt));
-    cout << tree[rt].l << ":" << tree[rt].r << " ->" << lc(rt) << " " << rc(rt) << endl;
-}
-void SegmentTree_Build(int rt, int l, int r)
-{
-    l(rt) = l, r(rt) = r;
-    if (l == r)
-    {
-        lc(rt) = rc(rt) = weight[l], cnt(rt) = 1;
-        return;
-    }
-    int mid = (l + r) >> 1;
-    SegmentTree_Build(ls(rt), l, mid);
-    SegmentTree_Build(rs(rt), mid + 1, r);
-    PushUp(rt);
-    //向上更新
-}
-void Update(int rt, int l, int r, int d)
-{
-    if (l <= l(rt) && r(rt) <= r)
-    {
-        lc(rt) = rc(rt) = d, cnt(rt) = 1;
-        return;
-    }
-    int mid = (l(rt) + r(rt)) >> 1;
-    if (l <= mid)
-        Update(ls(rt), l, r, d);
-    if (r > mid)
-        Update(rs(rt), l, r, d);
-    PushUp(rt);
-}
-ll Query(int rt, int l, int r)
-{
-    if (l(rt) >= l && r(rt) <= r)
-    {
-        return cnt(rt);
-    }
-    ll ret = 0;
-    int mid = (l(rt) + r(rt)) >> 1;
-    if (l <= mid)
-        ret += Query(ls(rt), l, r);
-    if (r > mid)
-        ret += Query(rs(rt), l, r);
-    return ret;
-}
-/*
-    重链剖分
-        fa[x] 表示节点x在树上的父亲
-        dep[x]表示节点x在树上的深度
-        sze[x]表示节点x的子树的节点个数
-        son[x]表示节点x的重儿子
-        top[x]表示节点x所在重链的顶部节点（深度最小）
-        dfn[x]表示节点x的DFS序 ，也是其在线段树中的编号
-        rnk[x]表示DFS序所对应的节点编号，有rnk[dfn[x]]=x
- */
-void Add(int x, int y)
-{
-    ver[++tot] = y, nxt[tot] = head[x], head[x] = tot;
-}
-void Build(int x, int pre, int depth)
-{
-    son[x] = -1, sze[x] = 1, dep[x] = depth;
-    for (int i = head[x]; i; i = nxt[i])
-    {
-        int y = ver[i];
-        if (y == pre)
-            continue;
-        Build(y, x, depth + 1);
-        sze[x] += sze[y], fa[y] = x;
-        if (son[x] == -1 || sze[son[x]] < sze[y])
-            son[x] = y;
-    }
-}
-void Decomposition(int x, int t)
-{
-    top[x] = t, dfn[x] = ++dfn_tot, rnk[dfn_tot] = x;
-    if (son[x] == -1)
-        return;
-    Decomposition(son[x], t);
-    for (int i = head[x]; i; i = nxt[i])
-    {
-        int y = ver[i];
-        if (y != son[x] && y != fa[x])
-            Decomposition(y, y);
-    }
-}
-ll Query_Path(int x, int y)
-{
-    ll ret = 0;
-    while (top[x] != top[y])
-    {
-        if (dep[top[x]] < dep[top[y]])
-            swap(x, y);
-        ret += Query(1, dfn[top[x]], dfn[x]);
-        x = fa[top[x]];
-    }
-    if (dep[x] > dep[y])
-        swap(x, y);
-    ret += Query(1, dfn[x], dfn[y]);
-    return ret;
-}
-void Update_Path(int x, int y, int d)
-{
-    while (top[x] != top[y])
-    {
-        if (dep[top[x]] < dep[top[y]])
-            swap(x, y);
-        Update(1, dfn[top[x]], dfn[x], d);
-        x = fa[top[x]];
-    }
-    if (dep[x] > dep[y])
-        swap(x, y);
-    Update(1, dfn[x], dfn[y], d);
-}
+int a[N];
+ll dp[N][2];
 int main()
 {
-    int n, m;
-    scanf("%d%d", &n, &m);
-    for (int i = 1; i <= n; i++)
-        scanf("%d", &temp[i]);
-    int x, y;
-    for (int i = 1; i <= n - 1; i++)
-        scanf("%d%d", &x, &y), Add(x, y), Add(y, x);
-    Build(1, 0, 1);
-    Decomposition(1, 1);
-    for (int i = 1; i <= n; i++)
-        weight[i] = temp[rnk[i]], cout << "dfn " << i << "=" << rnk[i] << endl;
-    SegmentTree_Build(1, 1, dfn_tot);
-    for (int i = 1; i <= m; i++)
+    ll n;
+    scanf("%lld",&n);
+    for(int i=1;i<=n;i++)
+        scanf("%d",&a[i]),a[i]=(a[i]>0)?1:-1;
+    for(int i=1;i<=n;i++)
     {
-        char op[2];
-        scanf("%s", op);
-        if (op[0] == 'C')
+        if(a[i]>0)
         {
-            int a, b, c;
-            scanf("%d%d%d", &a, &b, &c);
-            Update_Path(a, b, c);
+            dp[i][0]=1,dp[i][1]=0;
+            dp[i][0]+=dp[i-1][0];
+            dp[i][1]+=dp[i-1][1];
         }
-        else if (op[0] == 'Q')
+        else
         {
-            int a, b;
-            scanf("%d%d", &a, &b);
-            printf("%lld\n", Query_Path(a, b));
+            dp[i][0]=0,dp[i][1]=1;
+            dp[i][0]+=dp[i-1][1];
+            dp[i][1]+=dp[i-1][0];
         }
     }
-    system("pause");
+    ll sum=n*(n+1)/2;
+    ll ans=0;
+    for(int i=1;i<=n;i++)
+        ans+=dp[i][0];
+    printf("%lld %lld\n",sum-ans,ans);
+    //system("pause");
     return 0;
 }
--- 杂项/常用数学知识.cpp
+++ 杂项/常用数学知识.cpp
@@ -3,22 +3,65 @@
     裴蜀定理
     对于任意整数a,b，存在一对整数x,y，满足ax+by=gcd(a,b)。
     d=gcd(a,b)
+    ax+by=c 当且仅当d|c时有解
+    ax+by=c 通解:
+    x=(c/d)*x0+k*(b/d)
+    y=(c/d)*y0-k*(a/d)
+    k∈Z,x0,y0为ax+by=gcd(a,b)的一组特解 
 */
 #define ll long long
 ll exgcd(ll a, ll b, ll &x, ll &y)
 {
-    if (b == 0)
+    if (!b)
     {
         x = 1, y = 0;
         return a;
     }
-    ll d = exgcd(b, a % b, x, y);
-    ll z = x;
-    x = y;
-    y = z - y * (a / b);
+    ll d = exgcd(b, a % b, x, y), z = x;
+    x = y, y = z - y * (a / b);
     return d;
 }
-
+/*
+    乘法逆元
+    a*x≡1(mod m) a,m互质
+    a^(p-2)=inv(a,p) p为质数,a<p(a%=p)
+*/
+//线性递推
+inv[1] = 1;
+for (int i = 2; i < N; i++)
+    inv[i] = (p - p / i) * inv[p % i] % p;
+//阶乘逆元 组合数取模
+ll fac[N];//阶乘
+ll inv[N];//阶乘逆元
+ll C(ll a,ll b)
+{
+    if(b>a) return 0;
+    if(b==0) return 1;
+    return fac[a]*inv[b]%mod*inv[a-b]%mod;
+}
+void init()
+{
+    fac[0]=1;
+    for(int i=1;i<N;i++)
+    {
+        fac[i]=(fac[i-1]*i)%mod;
+    }
+    inv[N-1]=quick_pow(fac[N-1],mod-2);//费马小定理 求逆元a^(p-1)≡1(mod p)的逆元为a^(p-2)
+    for(int i=N-2;i>=0;i--)
+    {
+        inv[i]=inv[i+1]*(i+1)%mod;
+    }
+}
+//exgcd
+ll inv(ll a, ll m)
+{
+    ll x, y;
+    exgcd(a, m, x, y);
+    return (x % m + m) % m;
+}
+/*
+    中国剩余定理
+*/
 ll a[N], m[N];
 // x≡a(mod m)
 // TODO 可能溢出 参考2009年国家集训队论文，骆可强：《论程序底层优化的一些方法与技巧》
